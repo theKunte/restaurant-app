134bfe1fd5feb84e27cdd62785a9ca98c524c59b
Jenny <jennykunte90@gmail.com>
2023-10-02 22:29:09 -0700

    added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import Header from "./components/Header.js";
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Header />
     </div>
   );
 }
